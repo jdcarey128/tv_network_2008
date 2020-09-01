85f16a5db5030cf773e8d2dc845c20fb12b7e0d1
Joshua Carey <jdcarey128@gmail.com>
2020-09-01 10:00:25 -0600

    Add main_characters test and method

--- lib/network.rb
+++ lib/network.rb
@@ -7,7 +7,16 @@ class Network
   end
 
   def add_show(show)
-    @shows << show 
+    @shows << show
+  end
+
+  def main_characters
+    main_characters = @shows.map do |show|
+      show.characters.find_all do |character|
+        character.name == character.name.upcase && character.salary > 500_000
+      end
+    end
+    main_characters.flatten
   end
 
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -36,4 +36,12 @@ class NetworkTest < Minitest::Test
     assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
   end
 
+  def test_it_can_identify_main_characters
+
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@kitt], @nbc.main_characters
+  end
+
 end
