976a5903ab546c6279a41616df00e59c35c9ee49
Joshua Carey <jdcarey128@gmail.com>
2020-09-01 09:37:02 -0600

    Add network class and attributes

---
+++ test/network_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+
+  def setup
+
+    @nbc = Network.new("NBC")
+  end
+
+  def test_it_exists
+
+    assert_instance_of Network, @nbc
+  end
+
+  def test_it_has_attributes
+
+    assert_equal "NBC", @nbc.name
+    assert_equal [], @nbc.shows 
+  end
+
+end
