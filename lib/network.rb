605fd5ec283301223298be2d83623abc81cb0f0a
Joshua Carey <jdcarey128@gmail.com>
2020-09-01 09:41:34 -0600

    Create add_show test and method

--- lib/network.rb
+++ lib/network.rb
@@ -1,9 +1,13 @@
 class Network
-  attr_reader :name, :shows 
+  attr_reader :name, :shows
 
   def initialize(name)
     @name = name
     @shows = []
+  end
+
+  def add_show(show)
+    @shows << show 
   end
 
 end
